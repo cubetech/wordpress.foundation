01336ef92456e15595368397bf949d259e3ac819
Misbachul Munir <misbcl_munir@yahoo.com>
2012-07-14 07:59:34 +0700

    Fix viewport definition error on Chrome I got "Viewport argument value "device-width;" for key "width" not recognized. Content ignored." from Chrome. After some googling I found out that comma are the correct delimiters for viewport properties. This commit fix this problem by replacing colon with comma.
    Ref:
    http://stackoverflow.com/questions/5555125/viewport-tag-syntax
    https://github.com/h5bp/html5-boilerplate/issues/626
    http://drupal.org/node/1292710
    
    Signed-off-by: Misbachul Munir <misbcl_munir@yahoo.com>

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 		
 		<title><?php wp_title('', true, 'right'); ?></title>
 				
-		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		
 		<!-- icons & favicons -->
 		<!-- For iPhone 4 -->
