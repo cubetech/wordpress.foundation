b7c7d3f3fd8ebbd5dd06d2dd9dd2a71bc67f46e6
christoph.ackermann <info@acki.be>
2012-12-20 21:33:09 +0100

    modified config for new structure

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'zurb-foundation'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "app/css/vendor"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "app/img/vendor"
+javascripts_dir = "app/javascript/vendor/"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
