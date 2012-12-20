12fe26a091e26548e215bdb412ecfcf15061c817
christoph.ackermann <info@acki.be>
2012-12-20 21:37:10 +0100

    modified structure hard

--- config.rb
+++ config.rb
@@ -5,12 +5,13 @@ require 'zurb-foundation'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "assets/css/vendor"
-sass_dir = "sass"
+sass_dir = "assets/sass/vendor"
 images_dir = "assets/img/vendor"
-javascripts_dir = "assets/js/vendor/"
+javascripts_dir = "assets/js/vendor"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
